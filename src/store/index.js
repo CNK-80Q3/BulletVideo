f341d5cbc0d45413f1eb0d8baa26dcffbbc78de9
cnk80q3 <947301649@qq.com>
2020-04-15 17:06:08 +0800

    Organize project directory.

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import mutations from "./mutations";
+import actions from "./actions";
+import getters from "./getters";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  mutations: mutations,
+  actions: actions,
+  modules: getters
 });
---
+++ src/views/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="home"></div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
+
+<style>
+#home {
+  width: 100vw;
+  height: 100vh;
+  background-color: antiquewhite;
+}
+</style>
